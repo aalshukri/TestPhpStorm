98ab54b3cbba62712456cc5ab609ae4cf237d657
aalshukri <aalshukri@googlemail.com>
2020-05-20 15:43:05 +0100

    Updated php file with content to add breakpoint for debug testing.

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,2 +1,10 @@
 <?php
-echo "Hello World";
+echo "Hello World" . "\n</br>";
+
+$name = 'Ash';
+$fruits = array('apple', 'pear', 'lemon');
+
+foreach ($fruits as $fruit){
+    $output = $name . " likes " . $fruit . "\n</br>";
+    echo $output;
+}
